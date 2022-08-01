dd5f8a00c89799540121c193a2a18c44aa2d1ceb
bigbit99 <108947985+bigbit99@users.noreply.github.com>
2022-08-01 22:07:07 +0900

    Update Todo.jsx

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,11 +1,13 @@
 
 
 const Todo = ({todoProps}) => {
-
+    //비구조화 할당 
+    const {title, content} = todoProps 
+    
     return (
         <div style={{border : '1px solid black'}}>
-            <p>{todoProps.title}</p>
-            <span>{todoProps.content}</span>
+            <p>{title}</p>
+            <span>{content}</span>
             <button>삭제하기</button>
             <button>완료</button>
         </div>
